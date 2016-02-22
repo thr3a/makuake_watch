e85c083d8faa0e2fe61dad750fccd3307c72d660
thr3a <12jk172@ms.dendai.ac.jp>
2016-02-22 20:51:42 +0900

    Projectモデルにバリデーションを追加した

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,11 @@
 class Project < ActiveRecord::Base
+  validates :id, presence: true, uniqueness: true
+  validates :title, presence: true
+  validates :genre, presence: true
+  validates :owner_id, presence: true
+  validates :content, presence: true
+  validates :money, presence: true, numericality: { only_integer: true }
+  validates :goal_money, presence: true, numericality: { only_integer: true }
+  validates :deadline, presence: true
+  validates :supporter_num, presence: true
 end
