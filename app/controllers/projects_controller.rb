f55f82a9e5e1349b4da6a73862425331a35616d4
thr3a <12jk172@ms.dendai.ac.jp>
2016-03-02 23:04:11 +0900

    グラフに表示する項目を１日１つに変更した

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,16 +9,12 @@ class ProjectsController < ApplicationController
   # GET /project/:id
   def show
     @project = Project.find_by id: params[:id]
-    hoge = ProjectProgress.where(project_id: params[:id]).order(:date)
-    @money = hoge.pluck(:money)
-    @supporter_num = hoge.pluck(:supporter_num)
-    @date = hoge.pluck(:date).map {|d|
-      if d.hour == 0
-        d.strftime('%-m月%d日 %H')
-      else
-        d.strftime('%H')
-      end
-    }
+    pp1 = ProjectProgress.where(project_id: params[:id]).first(3).map{|a|a.attributes}
+    pp2 = ProjectProgress.where("project_id= ? AND hour(created_at) = ?", params[:id], 15).map{|a|a.attributes}
+    pp = pp1.concat pp2
+    @money = pp.map{|a|a["money"]}
+    @supporter_num = pp.map{|a|a["supporter_num"]}
+    @date = pp.map{|a|a["date"].strftime('%-m月%d日 %H時')}
 end
 
   private
